ac73082bf8d104d4a04aff08d260837c81891b5e
Simon Ho <simon@strongloop.com>
2015-06-18 14:44:34 -0700

    Update try-demo.cmd to use PM

--- notes-app-gateway/try-demo.cmd
+++ notes-app-gateway/try-demo.cmd
@@ -2,12 +2,17 @@ cd api-server
 call npm i
 cd ..\web-server
 call npm i
-cd ..\gateway-server
+cd ../gateway-server
 call git submodule init
 call git submodule update
-cd ..\sample-configs\phase-4
+cd ..\sample-configs\pm-demo
 call copy-files
 cd ..\..\gateway-server
 call npm i
-cd ..
+call slc start
+call slc ctl set-size 1 1
+cd ..\api-server
+call slc start
+call slc ctl set-size 2 1
+cd ..\web-server
 call node .
